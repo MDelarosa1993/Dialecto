a09ec4c4daac5e829f026e01aef7811a8c47b4c3
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-01-19 20:00:16 -0500

    added tailwind css to app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,19 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('tailwindcss'),
+            options: {
+              config: './tailwind.config.js',
+            },
+          },
+          require('autoprefixer'),
+        ],
+      },
+    },
   });
 
   return app.toTree();
