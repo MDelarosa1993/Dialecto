9a635f27a4b22f3908af68a2fdc684b18e80f312
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-02-28 15:50:18 -0500

    Created authservice to handle login and register

---
+++ src/app/services/auth-service.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthServiceService {
+  private baseUrl = 'http:localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  register(userData: { email: string; password: string }): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, userData);
+  }
+
+  login(credentials: { email: string; password: string }): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, credentials);
+  }
+}
