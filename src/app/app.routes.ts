db0c13d8e962b5ce9a3a2264e31a24108edf6b95
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-02-28 16:50:41 -0500

    Setup Routing to login and register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,19 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [{
-    path: '',
-    component: LandingPageComponent
-}];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+];
